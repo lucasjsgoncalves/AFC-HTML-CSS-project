66050181bdc93c697b0ba612cc2e51abefca0e71
lucasjsgoncalves <lucasjsgoncalves@gmail.com>
2023-02-03 16:40:13 -0300

    Added new media queries

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const times = document.querySelector(".images_qbs");
+const players = document.querySelectorAll(".imgs-players-link");
 
+times.addEventListener("mouseout", function (e) {
+  players.forEach((el) => {
+    el.classList.remove("grey-out");
+  });
+});
+
+times.addEventListener("mouseover", function (e) {
+  if (!e.target.closest(".imgs-players-link")) return;
+
+  players.forEach((el) => {
+    if (el.dataset !== e.target.closest(".imgs-players-link").dataset) {
+      el.classList.add("grey-out");
+    }
+  });
+});
